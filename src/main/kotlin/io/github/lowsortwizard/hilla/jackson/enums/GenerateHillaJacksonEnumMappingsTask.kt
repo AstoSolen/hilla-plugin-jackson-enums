71b0157e93dae6c71bc067f7541f83d2ea86770e
dmitriev <a.dmitriev@develonica.ru>
2026-05-23 19:13:32 +0400

    init

--- src/main/kotlin/io/github/lowsortwizard/hilla/jackson/enums/GenerateHillaJacksonEnumMappingsTask.kt
+++ src/main/kotlin/io/github/lowsortwizard/hilla/jackson/enums/GenerateHillaJacksonEnumMappingsTask.kt
@@ -1,4 +1,4 @@
-package io.github.astosolen.hilla.jackson.enums
+package io.github.lowsortwizard.hilla.jackson.enums
 
 import groovy.json.JsonOutput
 import org.gradle.api.DefaultTask
--- src/main/kotlin/io/github/lowsortwizard/hilla/jackson/enums/HillaJacksonEnumsPlugin.kt
+++ src/main/kotlin/io/github/lowsortwizard/hilla/jackson/enums/HillaJacksonEnumsPlugin.kt
@@ -1,4 +1,4 @@
-package io.github.astosolen.hilla.jackson.enums
+package io.github.lowsortwizard.hilla.jackson.enums
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
